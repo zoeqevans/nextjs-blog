9c55ebf251a7140b022f55e4d475dfafc8d95e95
montyevans <montyevans@gmail.com>
2022-09-02 02:26:44 -0400

    Vercel fixes

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
+export const siteTitle = "Monty Evans";
+
 export default function Layout({ children, meta }) {
   return (
     <div>
