0ecf1da0ae1745cd45ce5da01368069ebc62e9e5
montyevans <montyevans@gmail.com>
2024-08-11 20:45:45 -0400

    Fixing import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Script from "next/script";
 import Link from "next/link";
 import Date from "../components/date";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { usePersonalDetails } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
